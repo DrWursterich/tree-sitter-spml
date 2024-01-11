01ed3af0eafe777f91ab3cdb94fec40c01efddb1
Mario Schäper <schaeper@sitepark.com>
2024-01-11 18:34:18 +0100

    build: add bindings for c

---
+++ bindings/c/spml.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_SPML_H_
+#define TREE_SITTER_SPML_H_
+
+#include <tree_sitter/parser.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_spml();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_SPML_H_
