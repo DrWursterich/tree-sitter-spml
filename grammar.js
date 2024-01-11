7c38a81253353a8d3e3047fca038ae2a80830cc2
DrWursterich <mario.schaeper@web.de>
2024-01-11 02:02:56 +0100

    feat: added header, include, loop, map, print and set tags

--- grammar.js
+++ grammar.js
@@ -15,31 +15,57 @@ module.exports = grammar({
 
 	rules: {
 
-		document: $ => repeat(
-			$._top_level_tag,
+		document: $ => seq(
+			$.page_header,
+			repeat1($.taglib_header),
+			repeat($._top_level_tag),
 		),
 
 		_top_level_tag: $ => choice(
-			$.include_tag,
-			$.if_tag,
+			$.collection_tag,
 			$.condition_tag,
+			$.if_tag,
+			$.include_tag,
+			$.loop_tag,
+			$.map_tag,
+			$.print_tag,
+			$.set_tag,
 			$.text,
 		),
+
+		page_header: $ => seq(
+			$.header_open,
+			'page',
+			$.language_attribute,
+			$.pageEncoding_attribute,
+			$.contentType_attribute,
+			$.header_close,
+		),
+
+		taglib_header: $ => seq(
+			$.header_open,
+			'taglib',
+			choice(
+				$.uri_attribute,
+				$.tagdir_attribute,
+			),
+			$.prefix_attribute,
+			$.header_close,
+		),
+
+		header_open: $ => '<%@',
+		header_close: $ => '%>',
 
 		// tags
 		
 		argument_tag: $ => seq(
 			$.argument_tag_open,
 			$.name_attribute,
-			optional($.default_attribute),
 			optional($.locale_attribute),
 			choice(
 				seq(
 					$.value_attribute,
-					$.self_closing_tag_end,
-				),
-				seq(
-					$.text_attribute,
+					optional($.default_attribute),
 					$.self_closing_tag_end,
 				),
 				seq(
@@ -55,8 +81,14 @@ module.exports = grammar({
 					$.self_closing_tag_end,
 				),
 				seq(
+					optional($.default_attribute),
 					'>',
-					$.text,
+					repeat(
+						choice(
+							$.text,
+							$.print_tag,
+						),
+					),
 					$.argument_tag_close,
 				),
 			),
@@ -64,16 +96,74 @@ module.exports = grammar({
 		argument_tag_open: $ => '<sp:argument',
 		argument_tag_close: $ => '</sp:argument>',
 
+		collection_tag: $ => seq(
+			$.collection_tag_open,
+			$.name_attribute,
+			optional(
+				repeat1(
+					choice(
+						$.locale_attribute,
+						$.publisher_attribute,
+						$.scope_attribute
+					),
+				),
+			),
+			choice(
+				seq(
+					$.action_attribute,
+					optional($.index_attribute),
+					$.value_attribute,
+					optional($.default_attribute),
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.action_attribute,
+					optional($.index_attribute),
+					$.object_attribute,
+					optional($.default_attribute),
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.action_attribute,
+					optional($.index_attribute),
+					$.expression_attribute,
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.action_attribute,
+					optional($.index_attribute),
+					$.condition_attribute,
+					$.self_closing_tag_end,
+				),
+				seq(
+					optional($.action_attribute),
+					$.query_attribute,
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.action_attribute,
+					optional($.index_attribute),
+					optional($.default_attribute),
+					'>',
+					repeat(
+						choice(
+							$.text,
+							$.print_tag,
+						),
+					),
+					$.collection_tag_close,
+				),
+			),
+		),
+		collection_tag_open: $ => '<sp:collection',
+		collection_tag_close: $ => '</sp:collection>',
+
 		condition_tag: $ => seq(
 			$.condition_tag_open,
 			'>',
 			$.if_tag,
-			repeat(
-				$.elseif_tag,
-			),
-			optional(
-				$.else_tag,
-			),
+			repeat($.elseif_tag),
+			optional($.else_tag),
 			$.condition_tag_close,
 		),
 		condition_tag_open: $ => '<sp:condition',
@@ -82,9 +172,7 @@ module.exports = grammar({
 		else_tag: $ => seq(
 			$.else_tag_open,
 			'>',
-			repeat(
-				$._top_level_tag,
-			),
+			repeat($._top_level_tag),
 			$.else_tag_close,
 		),
 		else_tag_open: $ => '<sp:else',
@@ -119,9 +207,7 @@ module.exports = grammar({
 			),
 			optional($.locale_attribute),
 			'>',
-			repeat(
-				$._top_level_tag,
-			),
+			repeat($._top_level_tag),
 			$.elseif_tag_close,
 		),
 		elseif_tag_open: $ => '<sp:elseif',
@@ -156,9 +242,7 @@ module.exports = grammar({
 			),
 			optional($.locale_attribute),
 			'>',
-			repeat(
-				$._top_level_tag,
-			),
+			repeat($._top_level_tag),
 			$.if_tag_close,
 		),
 		if_tag_open: $ => '<sp:if',
@@ -185,12 +269,167 @@ module.exports = grammar({
 		include_tag_open: $ => '<sp:include',
 		include_tag_close: $ => '</sp:include>',
 
+		loop_tag: $ => seq(
+			$.loop_tag_open,
+			choice(
+				$.collection_attribute,
+				seq(
+					$.list_attribute,
+					optional($.separator_attribute),
+				),
+			),
+			optional($.item_attribute),
+			optional($.locale_attribute),
+			'>',
+			repeat($._top_level_tag),
+			$.loop_tag_close,
+		),
+		loop_tag_open: $ => '<sp:loop',
+		loop_tag_close: $ => '</sp:loop>',
+
+		map_tag: $ => seq(
+			$.map_tag_open,
+			$.name_attribute,
+			$.action_attribute,
+			optional($.key_attribute),
+			optional($.locale_attribute),
+			optional($.scope_attribute),
+			optional($.overwrite_attribute),
+			choice(
+				seq(
+					optional(
+						choice(
+							seq(
+								choice(
+									$.value_attribute,
+									$.object_attribute,
+								),
+								optional($.default_attribute),
+							),
+							$.expression_attribute,
+							$.condition_attribute,
+						),
+					),
+					$.self_closing_tag_end,
+				),
+				seq(
+					optional($.default_attribute),
+					'>',
+					repeat(
+						choice(
+							$.text,
+							$.print_tag,
+						),
+					),
+					$.map_tag_close,
+				),
+			),
+		),
+		map_tag_open: $ => '<sp:map',
+		map_tag_close: $ => '</sp:map>',
+
+		set_tag: $ => seq(
+			$.set_tag_open,
+			$.name_attribute,
+			optional($.locale_attribute),
+			optional($.overwrite_attribute),
+			optional($.scope_attribute),
+			optional($.insert_attribute),
+			optional($.contentType_attribute),
+			choice(
+				seq(
+					$.value_attribute,
+					optional($.default_attribute),
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.expression_attribute,
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.condition_attribute,
+					$.self_closing_tag_end,
+				),
+				seq(
+					$.object_attribute,
+					optional($.default_attribute),
+					$.self_closing_tag_end,
+				),
+				seq(
+					optional($.default_attribute),
+					'>',
+					repeat(
+						choice(
+							$.text,
+							$.print_tag,
+						),
+					),
+					$.set_tag_close,
+				),
+			),
+		),
+		set_tag_open: $ => '<sp:set',
+		set_tag_close: $ => '</sp:set>',
+
+		print_tag: $ => seq(
+			$.print_tag_open,
+			choice(
+				seq(
+					$.name_attribute,
+					optional($.default_attribute),
+				),
+				seq(
+					$.text_attribute,
+					repeat($.arg_attribute),
+					optional($.default_attribute),
+				),
+				$.expression_attribute,
+				$.condition_attribute,
+			),
+			optional($.convert_attribute),
+			optional(
+				choice(
+					$.encoding_attribute,
+					$.decoding_attribute,
+				),
+			),
+			optional(
+				seq(
+					choice(
+						$.encrypt_attribute,
+						$.decrypt_attribute
+					),
+					optional($.cryptkey_attribute),
+				),
+			),
+			optional($.locale_attribute),
+			optional($.decimalformat_attribute),
+			$.self_closing_tag_end,
+		),
+		print_tag_open: $ => '<sp:print',
+		// print_tag_close: $ => '</sp:print>',
+
 		self_closing_tag_end: $ => '/>',
 
 		// attributes
 		
+		action_attribute: $ => seq(
+			'action=',
+			$.string,
+		),
+
 		anchor_attribute: $ => seq(
 			'anchor=',
+			$.string,
+		),
+
+		arg_attribute: $ => seq(
+			'arg=',
+			$.string,
+		),
+
+		collection_attribute: $ => seq(
+			'collection=',
 			$.string,
 		),
 
@@ -204,8 +443,48 @@ module.exports = grammar({
 			$.string,
 		),
 
+		contentType_attribute: $ => seq(
+			'contentType=',
+			$.string,
+		),
+
+		convert_attribute: $ => seq(
+			'convert=',
+			$.string,
+		),
+
+		cryptkey_attribute: $ => seq(
+			'cryptkey=',
+			$.string,
+		),
+
+		decimalformat_attribute: $ => seq(
+			'decimalformat=',
+			$.string,
+		),
+
+		decoding_attribute: $ => seq(
+			'decoding=',
+			$.string,
+		),
+
+		decrypt_attribute: $ => seq(
+			'decrypt=',
+			$.string,
+		),
+
 		default_attribute: $ => seq(
 			'default=',
+			$.string,
+		),
+
+		encoding_attribute: $ => seq(
+			'encoding=',
+			$.string,
+		),
+
+		encrypt_attribute: $ => seq(
+			'encrypt=',
 			$.string,
 		),
 
@@ -234,8 +513,38 @@ module.exports = grammar({
 			$.string,
 		),
 
+		index_attribute: $ => seq(
+			'index=',
+			$.string,
+		),
+
+		insert_attribute: $ => seq(
+			'insert=',
+			$.string,
+		),
+
 		isNull_attribute: $ => seq(
 			'isNull=',
+			$.string,
+		),
+
+		item_attribute: $ => seq(
+			'item=',
+			$.string,
+		),
+
+		key_attribute: $ => seq(
+			'key=',
+			$.string,
+		),
+
+		language_attribute: $ => seq(
+			'language=',
+			$.string,
+		),
+
+		list_attribute: $ => seq(
+			'list=',
 			$.string,
 		),
 
@@ -274,8 +583,48 @@ module.exports = grammar({
 			$.string,
 		),
 
+		overwrite_attribute: $ => seq(
+			'overwrite=',
+			$.string,
+		),
+
+		pageEncoding_attribute: $ => seq(
+			'pageEncoding=',
+			$.string,
+		),
+
+		prefix_attribute: $ => seq(
+			'prefix=',
+			$.string,
+		),
+
+		publisher_attribute: $ => seq(
+			'publisher=',
+			$.string,
+		),
+
+		query_attribute: $ => seq(
+			'query=',
+			$.string,
+		),
+
 		return_attribute: $ => seq(
 			'return=',
+			$.string,
+		),
+
+		scope_attribute: $ => seq(
+			'scope=',
+			$.string,
+		),
+
+		separator_attribute: $ => seq(
+			'separator=',
+			$.string,
+		),
+
+		tagdir_attribute: $ => seq(
+			'tagdir=',
 			$.string,
 		),
 
